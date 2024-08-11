6a2683b06a78394282d9a430120d67c4356d2a83
Harsha Ankad <harshaankad2003@gmail.com>
2024-08-11 10:01:03 +0530

    Update socket.jsx

--- src/context/socket.jsx
+++ src/context/socket.jsx
@@ -2,6 +2,8 @@ import io from "socket.io-client";
 
 const socket = io("https://sahaya-api.vercel.app", {
   withCredentials: true, // Include credentials such as cookies and authorization headers
+  methods: ["GET", "POST","PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 });
 
 export default socket;
